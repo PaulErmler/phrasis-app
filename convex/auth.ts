c54d3d1d52a20a3a2b2b8183c3cb835df2786afe
PaulErmler <ermler.paul@gmail.com>
2025-12-20 10:46:08 -0800

    updates betterauth for convex to version 0.10

--- convex/auth.ts
+++ convex/auth.ts
@@ -4,6 +4,7 @@ import { components } from "./_generated/api";
 import { DataModel } from "./_generated/dataModel";
 import { query } from "./_generated/server";
 import { betterAuth } from "better-auth";
+import authConfig from "./auth.config";
 
 const siteUrl = process.env.SITE_URL!;
 
@@ -13,14 +14,10 @@ export const authComponent = createClient<DataModel>(components.betterAuth);
 
 export const createAuth = (
   ctx: GenericCtx<DataModel>,
-  { optionsOnly } = { optionsOnly: false },
 ) => {
   return betterAuth({
     // disable logging when createAuth is called just to generate options.
     // this is not required, but there's a lot of noise in logs without it.
-    logger: {
-      disabled: optionsOnly,
-    },
     baseURL: siteUrl,
     database: authComponent.adapter(ctx),
     // Configure simple, non-verified email/password to get started
@@ -36,7 +33,10 @@ export const createAuth = (
     },
     plugins: [
       // The Convex plugin is required for Convex compatibility
-      convex(),
+      convex({
+        authConfig,
+        jwksRotateOnTokenGenerationError: true,
+      }),
     ],
   });
 };
