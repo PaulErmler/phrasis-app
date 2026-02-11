ebdf8380f8c059294404544e3eef442dcf36d7d7
franziska.hradilak <franziska.hradilak@student.hpi.uni-potsdam.de>
2026-02-11 16:26:03 +0100

    Add local linting setup

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,11 @@
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import pluginReact from "eslint-plugin-react";
 import { defineConfig } from "eslint/config";
-import nextCoreWebVitals from "eslint-config-next/core-web-vitals";
-import nextTypescript from "eslint-config-next/typescript";
-import convexPlugin from "@convex-dev/eslint-plugin";
 
 export default defineConfig([
-  ...nextCoreWebVitals,
-  ...nextTypescript,
-  ...convexPlugin.configs.recommended,
+  { files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
+  tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
 ]);
